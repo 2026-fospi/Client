891855eef09eba858d51c4f1c59b5a40d2ce2754
유섭 <yoonbk2001027@gmail.com>
2026-02-21 23:32:35 +0900

    인증 화면 라우팅 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import {
-  ExchangePage,
-  HoldingsPage,
-  EventsPage,
-  MyPage,
-} from './pages';
+import { LoginPage, SignupPage } from './pages';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<ExchangePage />} />
-          <Route path="holdings" element={<HoldingsPage />} />
-          <Route path="events" element={<EventsPage />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
